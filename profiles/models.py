a439c515a498cad4a68723ace3d9202872d1e4b3
Alexa Hendry <alexahendry88@gmail.com>
2023-04-06 21:36:50 +0000

    Create posts app, Post model, and PostSerializer

--- profiles/models.py
+++ profiles/models.py
@@ -4,6 +4,10 @@ from django.db.models.signals import post_save
 
 
 class Profile(models.Model):
+    """
+    Profile model, related to 'owner', i.e. a User instance.
+    Default image set so that we can always reference image.url.
+    """
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
