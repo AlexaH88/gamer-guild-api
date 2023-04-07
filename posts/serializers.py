f4c5b11a5c916474d055d2513ffc80d31584e51b
Alexa Hendry <alexahendry88@gmail.com>
2023-04-07 00:58:08 +0000

    Add following_id field to profiles serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Post
+from followers.models import Follower
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -10,6 +11,7 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    following_id = serializers.SerializerMethodField()
 
     def validate_image(self, value):
         # check for 2mb file size limit
@@ -31,9 +33,19 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+                ).first()
+            return following.id if following else None
+        return None
+
     class Meta:
         model = Post
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'title', 'content',
             'image', 'video', 'is_owner', 'profile_id', 'profile_image',
+            'following_id',
         ]
