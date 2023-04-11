2c00d32abc4e2f4faefeacfc64b43dbd8634e52b
Alexa Hendry <alexahendry88@gmail.com>
2023-04-11 14:08:37 +0000

    Import re in settings to fix client origin dev error

--- gamer_guild_api/settings.py
+++ gamer_guild_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -135,9 +136,7 @@ if 'CLIENT_ORIGIN' in os.environ:
     ]
 if 'CLIENT_ORIGIN_DEV' in os.environ:
     extracted_url = re.match(
-        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE
-    ).group(0)
-else:
+        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
