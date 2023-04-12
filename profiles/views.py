eb390adc7d6b85d1224c4708c398f45ec8d67016
Alexa Hendry <alexahendry88@gmail.com>
2023-04-12 20:15:36 +0000

    Update profile filter views to include owner__followed__owner__profile in order to display homepage filter correctly

--- profiles/views.py
+++ profiles/views.py
@@ -29,7 +29,8 @@ class ProfileList(generics.ListAPIView):
         'owner__followed__created_at',
     ]
     filterset_fields = [
-        'owner__following__followed__profile'
+        'owner__following__followed__profile',
+        'owner__followed__owner__profile',
     ]
 
 
