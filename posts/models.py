295b1272745caf6f932c62bb8349f439a955f3e5
Alexa Hendry <alexahendry88@gmail.com>
2023-04-14 14:46:06 +0000

    Change default image on postd and events models

--- posts/models.py
+++ posts/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_post_xh1cpv', blank=True
+        upload_to='images/', default='../default_post_m4aipf', blank=True
     )
     video = models.URLField(blank=True)
 
