b6ff880dfd98a6eb49f7320dd1700c55fc255fa8
Alexa Hendry <alexahendry88@gmail.com>
2023-04-18 21:43:25 +0000

    Add get_member_id code to groups serializers

--- groups/serializers.py
+++ groups/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Group
+from members.models import Member
 
 
 class GroupSerializer(serializers.ModelSerializer):
@@ -8,12 +9,22 @@ class GroupSerializer(serializers.ModelSerializer):
     """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    member_id = serializers.SerializerMethodField()
     events_count = serializers.ReadOnlyField()
     members_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_member_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            member = Member.objects.filter(
+                owner=user, group=obj.owner
+                ).first()
+            return member.id if member else None
+        return None
 
     class Meta:
         model = Group
