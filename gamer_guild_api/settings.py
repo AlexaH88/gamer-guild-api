dae4a868cf73f21e982a6219ebbab05d39ebad22
Alexa Hendry <alexahendry88@gmail.com>
2023-04-25 18:41:05 +0000

    Change DEBUG to DEBUG in settings

--- gamer_guild_api/settings.py
+++ gamer_guild_api/settings.py
@@ -78,8 +78,8 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
-# DEBUG = 'DEBUG' in os.environ
+# DEBUG = 'DEV' in os.environ
+DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
