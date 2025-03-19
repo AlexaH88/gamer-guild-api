380a9cab692b1c328b0f2f63cac275d91a712844
Alexa Hendry <alexahendry88@gmail.com>
2025-03-20 00:18:41 +0100

    Add middleware for all auth

--- gamer_guild_api/settings.py
+++ gamer_guild_api/settings.py
@@ -144,6 +144,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware'
 ]
 
 ROOT_URLCONF = 'gamer_guild_api.urls'
