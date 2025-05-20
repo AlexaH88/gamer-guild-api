d67bb0c8ea48c7c150f1f96ee8955faa9b3c6773
Alexa <alexahendry88@gmail.com>
2025-05-20 02:07:30 +0200

    Fix middleware issue

--- gamer_guild_api/settings.py
+++ gamer_guild_api/settings.py
@@ -145,7 +145,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware'
 ]
 
 ROOT_URLCONF = 'gamer_guild_api.urls'
